5ac3295508fd2304d325dd5bf56e4c0e74c2a831
sans084 <166106128+sans084@users.noreply.github.com>
2025-06-30 14:08:54 +0530

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,34 +1,6 @@
 let shop=document.getElementById('shop')
 
-let shopItemsData=[{
-    id:"gfdun",
-    name:"Casual Shirt",
-    price:45,
-    desc:"Loren ipsum dolor, sit amet consectetur adipiscing",
-    img:"images/img-1.jpg"
-},
-{
-    id:"nsdhk",
-    name:"Office Shirt",
-    price:100,
-    desc:"Loren ipsum dolor, sit amet consectetur adipiscing",
-    img:"images/img-2.jpg"  
-},
-{
-    id:"basdhau",
-    name:"TShirt",
-    price:25,
-    desc:"Loren ipsum dolor, sit amet consectetur adipiscing",
-    img:"images/img-3.jpg"
 
-},
-{
-    id:"kamsdhi",
-    name:"Men's Shirt",
-    price:300,
-    desc:"Loren ipsum dolor, sit amet consectetur adipiscing",
-    img:"images/img-4.jpg"
-}];
 
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
